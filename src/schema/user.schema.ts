a3e4770f6add60eff18d92bb5d1f2c72a053f95c
Ahoora Amini <ahoora.amini8@gmail.com>
2023-05-09 13:01:36 +0330

    initial user schema

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,15 +1,62 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { UserRole } from './user.enum';
+import * as bcrypt from 'bcrypt';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({required: true,})
   email: string;
 
+  @Prop({required: true,})
+  username: string;
+
+  @Prop({required: true,})
+  password: string;
+
+  @Prop({required: false })
+  phoneNumber: string;
+
+  @Prop(UserRole)
+  type: UserRole;
+
   @Prop()
-  password: number;
+  profilePic: string;
+
+  @Prop({required: false,})
+  description: string;
+
+  @Prop({required: true,})
+  city: string;
+
+  @Prop({ type: [String], default: [] })
+  followers: string[];
+
+  @Prop({ type: [String], default: [] })
+  followings: string[];
+
 }
 
+
 export const UserSchema = SchemaFactory.createForClass(User);
+
+async function generateHash(password: string) {
+  return bcrypt.hash(password, 11);
+}
+
+UserSchema.pre('save', function (next) {
+  const user = this;
+  if (user.isNew || user.isModified('password')) {
+    return generateHash(user.password)
+      .then((hash) => {
+        user.password = hash;
+        return next();
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  }
+  return next();
+});
