f621fbe73dd0e4f6841420c07a81235c770f0e4b
amir <mosayebiamirreza@gmail.com>
2023-05-13 16:49:50 +0330

    create post dto

--- src/post/post.dto.ts
+++ src/post/post.dto.ts
@@ -1,27 +1,46 @@
-import {
-  IsOptional,
-  IsString,
-  IsDate,
-  IsBoolean,
-  IsNotEmpty,
-  IsEnum,
-  MinLength,
-  MaxLength,
-} from 'class-validator';
+import { IsOptional, IsString, IsNotEmpty, IsArray } from 'class-validator';
 import { User } from '../user/user.schema';
 
 export class CreatePostDto {
-  userId: string;
-  image: string;
+  @IsString()
+  @IsNotEmpty()
+  userId!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  image!: string;
+
+  @IsString()
+  @IsOptional()
   text: string;
+
+  @IsArray()
+  @IsOptional()
   liked: User[];
+
+  @IsArray()
+  @IsOptional()
   comment: User[];
 }
 
 export class UpdatePostDto {
+  @IsString()
+  @IsOptional()
   userId: string;
+
+  @IsOptional()
+  @IsString()
   image: string;
+
+  @IsOptional()
+  @IsString()
   text: string;
+
+  @IsOptional()
+  @IsArray()
   liked: User[];
+
+  @IsOptional()
+  @IsArray()
   comment: User[];
 }
