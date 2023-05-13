a4eee8ead5002ee24468f5f2260e45e471989ca9
amir <mosayebiamirreza@gmail.com>
2023-05-13 23:49:58 +0330

    unique email

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -7,7 +7,7 @@ export type UserDocument = HydratedDocument<User>;
 
 @Schema()
 export class User {
-  @Prop({ required: true })
+  @Prop({ required: true, unique: true })
   email: string;
 
   @Prop({ required: true, unique: true })
