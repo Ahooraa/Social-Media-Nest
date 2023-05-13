0aa9f286a271e0a58f750b077cd933bb441c1c06
amir <mosayebiamirreza@gmail.com>
2023-05-13 23:07:29 +0330

    post CRUD

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,6 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostService } from './post.service';
-import { CreatePostDto } from './post.dto';
+import { CreatePostDto, UpdatePostDto } from './post.dto';
 
 @Controller('post')
 export class PostController {
@@ -8,5 +16,26 @@ export class PostController {
   @Post('newpost')
   async newPost(@Body() Body: CreatePostDto): Promise<any> {
     return await this.postService.createPost(Body);
+  }
+
+  @Delete(':id/delete')
+  deletePost(@Param('id') id: string) {
+    return this.postService.deletePost(id);
+  }
+
+  @Patch(':id/update')
+  updatePost(@Param('id') id: string, @Body() data: UpdatePostDto) {
+    return this.postService.updatePost(id, data);
+  }
+
+  @Get(':id/one')
+  getOnePost(@Param('id') id: string) {
+    console.log(id);
+    return this.postService.getOnePost(id);
+  }
+
+  @Get('All')
+  getAllPost() {
+    return this.postService.getAllPost();
   }
 }
