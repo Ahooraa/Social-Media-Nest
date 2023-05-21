374fdb92aa5ecef1f02295ac7acc3dd55ed2026b
Ahoora Amini <ahoora.amini8@gmail.com>
2023-05-21 18:45:45 +0330

    changed signup/login directory

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Param,
+  Patch,
+  Delete,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto, loginDto } from '../user/user.dto';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
-@Controller('auth')
-export class AuthController {}
+@Controller('api/auth')
+export class AuthController {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  @Post('signup')
+  async createUser(@Body() userData: CreateUserDto) {
+    console.log(userData);
+    return await this.userService.createUser(userData);
+  }
+
+  @Post('login')
+  async login(@Body() body: loginDto): Promise<{ access_token: string }> {
+    const user = await this.userService.findUserByUsername(body.username);
+    console.log(user);
+
+    if (!user) {
+      throw new UnauthorizedException('username or password is incorrect!');
+    }
+
+    const comparePass = await bcrypt.compare(body.password, user.password);
+    if (!comparePass) {
+      throw new UnauthorizedException('username or password is incorrect!');
+    }
+    return { access_token: this.jwtService.sign({ id: user.id }) };
+  }
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -16,29 +16,9 @@ import { User } from './user.schema';
 
 @Controller('api/user')
 export class UserController {
-  constructor(private readonly userService: UserService, private readonly jwtService: JwtService) {}
+  constructor(private readonly userService: UserService, ) {}
 
-  @Post()
-  async createUser(@Body() userData: CreateUserDto) {
-    console.log(userData);
-    return await this.userService.createUser(userData);
-  }
-
-  @Post('login')
-  async login(@Body() body: loginDto): Promise<{ access_token: string }> {
-    const user = await this.userService.findUserByUsername(body.username);
-    console.log(user);
-
-    if (!user) {
-      throw new UnauthorizedException('username or password is incorrect!');
-    }
-    
-    const comparePass = await bcrypt.compare(body.password, user.password);
-    if (!comparePass) {
-      throw new UnauthorizedException('username or password is incorrect!');
-    }
-    return { access_token: this.jwtService.sign({ id: user.id }) };
-  }
+  
 
   @Get('username/:username')
   async getUserWithUsername(@Param('username') username: string): Promise<User> {
